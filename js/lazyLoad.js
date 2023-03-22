00b6e7ca0f2e2b42210cbaa36d5a607927d98854
GJ-Lin <1426955281@qq.com>
2023-03-23 00:57:51 +0800

    Site updated: 2023-03-23 00:57:50

--- js/lazyLoad.js
+++ js/lazyLoad.js
@@ -1,16 +1,30 @@
+const screenWidth = window.innerWidth;
+let imageUrlSuffix = '';
+
+if (screenWidth < 480) {
+  //设置小屏幕下的图片 url
+  imageUrlSuffix = '-small.jpeg';
+} else if (screenWidth < 768) {
+  //设置中等屏幕下的图片 url
+  imageUrlSuffix = '-medium.jpeg';
+} else {
+  //设置大屏幕下的图片 url
+  imageUrlSuffix = '-large.jpeg';
+}
+
 let imgs = document.querySelectorAll('img[data-src]');
 console.log(imgs);
 // 判断是否有图片
 if (imgs.length !== 0) {
 // 防止图片同时加载
-    // imgs[0].setAttribute('src', 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==');
-    if (imgs[0].getAttribute('data-src').endsWith('-small.jpeg')) {
-        imgs[0].setAttribute('src', imgs[0].getAttribute('data-src').replace('-small.jpeg', '-empty.png'));
-    } else {
-        imgs[0].setAttribute('src', imgs[0].getAttribute('data-src').replace('.jpeg', '-empty.png'));
-    }
-    imgs[0].setAttribute('opacity', '0');
-    imgs[0].setAttribute('transition', 'opacity 1s ease');
+    imgs[0].setAttribute('src', 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==');
+    // if (imgs[0].getAttribute('data-src').endsWith('-small.jpeg')) {
+    //     imgs[0].setAttribute('src', imgs[0].getAttribute('data-src').replace('-small.jpeg', '-empty.png'));
+    // } else {
+    //     imgs[0].setAttribute('src', imgs[0].getAttribute('data-src').replace('.jpeg', '-empty.png'));
+    // }
+    // imgs[0].setAttribute('opacity', '0');
+    imgs[0].setAttribute('transition', 'opacity 2s ease');
     imgs[0].setAttribute('width', '100px')
     imgs[0].setAttribute('height', '100px')
 }
@@ -25,20 +39,20 @@ let io = new IntersectionObserver(function (entires) {
         //     return;
         // }
         if (!oImg.hasAttribute('src')) {
-            // oImg.setAttribute('src', 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==');
-            if (oImg.getAttribute('data-src').endsWith('-small.jpeg')) {
-                oImg.setAttribute('src', oImg.getAttribute('data-src').replace('-small.jpeg', '-empty.png'));
-            } else {
-                oImg.setAttribute('src', oImg.getAttribute('data-src').replace('.jpeg', '-empty.png'));
-            }
-            oImg.setAttribute('opacity', '0');
-            oImg.setAttribute('transition', 'opacity 1s ease');
+            oImg.setAttribute('src', 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABQABh6FO1AAAAABJRU5ErkJggg==');
+            // if (oImg.getAttribute('data-src').endsWith('-small.jpeg')) {
+            //     oImg.setAttribute('src', oImg.getAttribute('data-src').replace('-small.jpeg', '-empty.png'));
+            // } else {
+            //     oImg.setAttribute('src', oImg.getAttribute('data-src').replace('.jpeg', '-empty.png'));
+            // }
+            // oImg.setAttribute('opacity', '0');
+            oImg.setAttribute('transition', 'opacity 2s ease');
             oImg.setAttribute('width', '100px')
             oImg.setAttribute('height', '100px')
         }
         // 当图片进入视口的时候，就赋值图片的真实地址
         if (item.intersectionRatio > 0 && item.intersectionRatio <= 1) {
-            oImg.setAttribute('src', oImg.getAttribute('data-src'))
+            oImg.setAttribute('src', oImg.getAttribute('data-src').replace('.jpeg', imageUrlSuffix));
             // oImg.classList.add('lazy-loaded');
             // oImg.classList.remove('lazy');
             io.unobserve(oImg);
